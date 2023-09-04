a37b3f788dff3d8f764ccbd97f4c8924d68af0d6
mohamed96321 <atefm.me499@gmail.com>
2023-09-04 15:34:07 +0300

    New templating engine is added like .hbs, .pug

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+
+const app = express();
+const path = require('path');
+const favicon = require('serve-favicon');
+
+// Enable hbs file
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+// Enable pug file
+// app.set('view engine', 'pug');
+// app.set('views', 'views');
+
+const adminData = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
+
+app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const rootDir = require('../util/path.js');
+
+const adminData = require('./admin.js');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'AZUW | Shop', path: '/'});
+});
+
+module.exports = router;
