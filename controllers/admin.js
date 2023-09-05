a5773b08b3ebfa67bbb279edb086ed5af41dd237
mohamed96321 <atefm.me499@gmail.com>
2023-09-05 23:52:23 +0300

    Edit Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,11 @@
-const Product = require('../models/products.js');
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product',
+  res.render('admin/edit-product',
   { 
     pageTitle: 'AZUW Admin | Add Product', 
-    path: '/admin/add-product'
+    path: '/admin/add-product',
+    editing: false,
   });
 };
 
@@ -14,9 +15,45 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId, 
+    updatedTitle, 
+    updatedImageUrl, 
+    updatedDesc, 
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', { 
+      pageTitle: 'AZUW Admin | Edit Product', 
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  });
 };
 
 exports.getProduct = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,12 @@ router.get('/add-product', adminController.getAddProduct);
 // /admin/products => GET
 router.get('/products', adminController.getProduct);
 
+// /admin/edit-product => GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
