a268cf97ad9801ade6cd0006d269022538a83b71
mohamed96321 <atefm.me499@gmail.com>
2023-09-10 12:12:26 +0300

    Make an order

--- shop-app-postgreSQL/app.js
+++ shop-app-postgreSQL/app.js
@@ -9,6 +9,9 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
+
 
 // Config Ejs file
 app.set('view engine', 'ejs');
@@ -45,12 +48,15 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
 // .sync({force: true})
 .sync()
 .then(result => {
-  return User.findOne({where: {id: 1}});
+  return User.findByPk(1);
   // console.log(result);
 })
 .then(user => {
--- shop-app-postgreSQL/controllers/shop.js
+++ shop-app-postgreSQL/controllers/shop.js
@@ -109,16 +109,44 @@ exports.postCartDeleteProduct = (req, res, next) => {
   .catch(err => console.log(err));
 };
 
-exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'AZUW Store | My Orders', 
-  });
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+  .getCart()
+  .then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts();
+  })
+  .then(products => {
+    return req.user
+    .createOrder()
+    .then(order => {
+      return order.addProducts(
+        products.map(product => {
+        product.itemorder = { quantity: product.itemcart.quantity };
+        return product;
+      }));
+    })
+    .catch(err => console.log(err));
+  })
+  .then(result => {
+    return fetchedCart.setProducts(null);
+  })
+  .then(result => {
+    res.redirect('/orders');
+  })
+  .catch(err => console.log(err));
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'AZUW Store | Payment', 
-  });
+exports.getOrders = (req, res, next) => {
+  req.user
+  .getOrders({include: ['products']})
+  .then(orders => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'AZUW Store | My Orders', 
+      orders: orders
+    });
+  })
+  .catch(err => console.log(err));
 };
