6e866fcdafdd5118cedd851086f32d9ae16002ae
Mohamed Atef <61386878+mohamed96321@users.noreply.github.com>
2023-09-11 10:10:32 +0300

    Delete shop-app-graphQL directory

--- shop-app-graphQL/util/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('shop', 'postgres', '@#$Mo96321', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
-
-module.exports = sequelize;
